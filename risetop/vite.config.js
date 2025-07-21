c092404e70b79c3285b32fc92da416cb69bb8504
Nekitai <fajarap.0204@gmail.com>
2025-07-21 16:09:46 +0800

    deploy keempat

--- risetop/vite.config.js
+++ risetop/vite.config.js
@@ -7,4 +7,12 @@ export default defineConfig({
   plugins: [
     tailwindcss(),
     react()],
+    build: {
+      outDir: 'dist',
+      
+    },
+    base: '',
+    server: {
+      historyApiFallback: true,
+    }
 })
